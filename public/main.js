fb4d8d3c31ac00418b646e601640221c569050e7
Diboll <bdiboll@proforma.com>
2018-03-12 19:23:44 -0400

    learned JS

--- public/main.js
+++ public/main.js
@@ -1,10 +1,33 @@
-const main = () => {
-
- 
-
-  document.querySelector('h1').textContent += "";
+const UpdateName1 = () => {
+  let team1name = document.querySelector("#team1name").value;
+  document.querySelector("#Name1Team").textContent = team1name;
 }
 
+const Add1Team1 = () => {
+  let scoreone = document.querySelector("#scoreone").textContent;
+  scoreone++;
+  document.querySelector("#scoreone").textContent = scoreone;
+}
 
+const Sub1Team1 = () => {
+  let scoreone = document.querySelector("#scoreone").textContent;
+  scoreone--;
+  document.querySelector("#scoreone").textContent = scoreone;
+}
 
-document.addEventListener('DOMContentLoaded', main)
+const UpdateName2 = () => {
+  let team2name = document.querySelector("#team2name").value;
+  document.querySelector("#Name2Team").textContent = team2name;
+}
+
+const Add1Team2 = () => {
+  let scoretwo = document.querySelector("#scoretwo").textContent;
+  scoretwo++;
+  document.querySelector("#scoretwo").textContent = scoretwo;
+}
+
+const Sub1Team2 = () => {
+  let scoretwo = document.querySelector("#scoretwo").textContent;
+  scoretwo--;
+  document.querySelector("#scoretwo").textContent = scoretwo;
+}
